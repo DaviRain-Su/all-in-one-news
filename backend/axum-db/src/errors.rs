ee34be802197d2d9abbe788ddec9d3cfb159a5af
Davirain <davirain.yin@gmail.com>
2023-09-14 11:59:20 +0800

    Update errors.rs

--- backend/axum-db/src/errors.rs
+++ backend/axum-db/src/errors.rs
@@ -2,8 +2,6 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum Error {
-    #[error("invalid subscriber name Error: {0}")]
-    ValidateSubscriberNameError(String),
-    #[error("invalid subscriber email Error: {0}")]
-    ValidateSubscriberEmailError(String),
+    #[error("dummy error")]
+    Dummy,
 }
