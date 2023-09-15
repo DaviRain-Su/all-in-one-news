b2274906f063db00c93677a7fe2708b280b0795b
Davirain <davirain.yin@gmail.com>
2023-09-16 01:08:58 +0800

    Update story.rs

--- frontend/web/src/story.rs
+++ frontend/web/src/story.rs
@@ -51,20 +51,69 @@ pub struct StoryItem {
 pub fn StoryListing(cx: Scope, story: StoryItem) -> Element {
     let StoryItem {
         title,
+        url,
         by,
         score,
         time,
         kids,
+        id,
         ..
     } = story;
 
-    let comments = kids.len();
+    let url = url.as_deref().unwrap_or_default();
+    let hostname = url
+        .trim_start_matches("https://")
+        .trim_start_matches("http://")
+        .trim_start_matches("www.");
+    let score = format!("{score} {}", if *score == 1 { " point" } else { " points" });
+    let comments = format!(
+        "{} {}",
+        kids.len(),
+        if kids.len() == 1 {
+            " comment"
+        } else {
+            " comments"
+        }
+    );
+    let time = time.format("%D %l:%M %p");
 
-    render! {
+    cx.render(rsx! {
         div {
             padding: "0.5rem",
             position: "relative",
-            "{title} by {by} ({score}) {time} {comments}"
+            div {
+                font_size: "1.5rem",
+                a {
+                    href: url,
+                    "{title}"
+                }
+                a {
+                    color: "gray",
+                    href: "https://news.ycombinator.com/from?site={hostname}",
+                    text_decoration: "none",
+                    " ({hostname})"
+                }
+            }
+            div {
+                display: "flex",
+                flex_direction: "row",
+                color: "gray",
+                div {
+                    "{score}"
+                }
+                div {
+                    padding_left: "0.5rem",
+                    "by {by}"
+                }
+                div {
+                    padding_left: "0.5rem",
+                    "{time}"
+                }
+                div {
+                    padding_left: "0.5rem",
+                    "{comments}"
+                }
+            }
         }
-    }
+    })
 }
