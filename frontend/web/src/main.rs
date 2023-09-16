9a5fa282bf56e26c48d82a1e738d34e0528b6e67
Davirain <davirain.yin@gmail.com>
2023-09-16 16:12:10 +0800

    Update main.rs

--- frontend/web/src/main.rs
+++ frontend/web/src/main.rs
@@ -14,6 +14,25 @@ fn main() {
 
 // create a component that renders a div with the text "Hello, world!"
 fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            display: "flex",
+            flex_direction: "row",
+            width: "100%",
+            div {
+                width: "50%",
+                Stories {}
+            }
+            div {
+                width: "50%",
+                Preview {}
+            }
+        }
+    })
+}
+
+// New
+fn Stories(cx: Scope) -> Element {
     render! {
         StoryListing {
             story: StoryItem {
@@ -27,6 +46,50 @@ fn App(cx: Scope) -> Element {
                 time: chrono::Utc::now(),
                 kids: vec![],
                 r#type: "".to_string(),
+            }
+        }
+    }
+}
+
+// New
+#[derive(Clone, Debug)]
+enum PreviewState {
+    Unset,
+    Loading,
+    Loaded(StoryPageData),
+}
+
+// New
+fn Preview(cx: Scope) -> Element {
+    let preview_state = PreviewState::Unset;
+    match preview_state {
+        PreviewState::Unset => render! {
+            "Hover over a story to preview it here"
+        },
+        PreviewState::Loading => render! {
+            "Loading..."
+        },
+        PreviewState::Loaded(story) => {
+            let title = &story.item.title;
+            let url = story.item.url.as_deref().unwrap_or_default();
+            let text = story.item.text.as_deref().unwrap_or_default();
+            render! {
+                div {
+                    padding: "0.5rem",
+                    div {
+                        font_size: "1.5rem",
+                        a {
+                            href: "{url}",
+                            "{title}"
+                        }
+                    }
+                    div {
+                        dangerous_inner_html: "{text}",
+                    }
+                    // for comment in &story.comments {
+                        // Comment { comment: comment.clone() }
+                    // }
+                }
             }
         }
     }
